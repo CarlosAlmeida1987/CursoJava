9e8d2235e66c1707e472f84ea7747cc8a3069b3f
CarlosAlmeida1987 <trimetria@hotmail.com>
2021-04-15 14:46:28 -0300

    Inicio do Curso

---
+++ target/generated-sources/annotations/aula/iw/Calculos.java
@@ -0,0 +1,104 @@
+package aula.iw;
+
+import java.util.Scanner;
+
+public class Calculos {
+	
+//	1. Fatorial 3,4,5,6 e 7;
+//	2. Polinômios: 4x3 2x2 x+10, 3x4 9x3 9x2 x+9, x4 2x3	6x2	+1 e 10x4 5x3 4x2 x+4.
+//	3. Tabuada de 9 para as quatro operações.
+//	4. Converter 4.67 para 4, 5 para 5,33 e 8.92 para 8.
+//	5. Encontrar os números primos.
+//	6. Determinar o resto de 10 por 5, 5 por 2, 11 por 3, 11 por 2.
+//	7. Determinar 20% de 40, 15% de 15, 13% de 440, 67% de 40, 3,3% de 11. 
+	
+	public static int Fatorial(int num1, int num2){
+		
+		double tres;
+		double quatro;
+		double cinco;
+		double seis;
+		double sete;
+		
+		return (num1 + num2);
+	}
+	
+	public static int Polinomios(int num1, int num2){
+		
+		double zero;
+		double um;
+		
+		System.out.println("Polinomios");
+		
+		return (num1 + num2);
+	}
+
+	public static int Tabuada(int num1, int num2){
+	
+	double x = 1 + 9;
+	double z = 2 + 9;
+	double total = 3 + 9;
+	
+	System.out.println("Tabuada");
+	
+	return (num1 + num2);
+	}
+
+	public static int Converter(int num1, int num2){
+	
+	double tres = 3;
+	double quatro = 4;
+	double cinco = 5;
+	double seis = 6;
+	double sete = 7;
+	
+	System.out.println("Converter");
+	
+	return (num1 + num2);
+	}
+
+	public static int Primos(int num1, int num2){
+	
+	double tres = 3;
+	double quatro = 4;
+	double cinco = 5;
+	double seis = 6;
+	double sete = 7;
+	
+	System.out.println("Primos");
+	return (num1 + num2);
+	}
+
+	public static int Resto(int num1, int num2){
+	
+	double tres = 3;
+	double quatro = 4;
+	double cinco = 5;
+	double seis = 6;
+	double sete = 7;
+	
+	System.out.println("Resto");
+	return (num1 + num2);
+	}
+
+	public static int Posentagem(int num1, int num2){
+	
+	double tres = 3;
+	double quatro = 4;
+	double cinco = 5;
+	double seis = 6;
+	double sete = 7;
+	
+	System.out.println("Posentagem");
+	return (num1 + num2);
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(Fatorial(10,50));
+		
+		System.out.println("Calculos Matematicos");
+		
+	}	
+
+}
